dd50e2024315653f0381d0ec88475256b4dcd97e
João Pellacani <eng.joaopellacani@gmail.com>
2025-10-17 16:29:34 -0300

    Start Project

---
+++ utils/functions.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def rastrigin(x):
+    """
+    Função de Rasterigin para otimização.
+    x: array ou lista de variáveis (ax: [x1, x2, x3, ...])
+    Retorna o valor da função (enquanto menor, melhor para minimização)
+    """
+    A = 10
+    x = np.array(x)
+    n = len(x)
+    return A * n + np.sum(x**2 - A * np.cos(2 * np.pi * x))
