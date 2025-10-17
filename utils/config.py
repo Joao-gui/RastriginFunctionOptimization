78184be1c842ae745d13e86f63bcd3b7a5866f6a
João Pellacani <eng.joaopellacani@gmail.com>
2025-10-17 17:07:36 -0300

    Adicionado config.py

---
+++ utils/config.py
@@ -0,0 +1,15 @@
+"""
+Arquivo de configuração do projeto de otimização (DEAP + Rastrigin)
+"""
+
+# ====== PARÂMETROS DO AG ======
+POP_SIZE = 100      # Tamanho da população
+N_GEN = 50          # Número de gerações
+PROB_CX = 0.7       # Probabilidade de cruzamento
+PROB_MT = 0.2         # Probabilidade de mutação
+SEED = 42           # Semente aleatória (para reprodutibilidade)
+
+# ======= PARÂMETROS DO PROBLEMA ========
+IND_SIZE = 5        # Dimensão de indivíduo (número de variáveis)
+BOUND_LOW = -5.12   # Limite inferior
+BOUND_UP = 5.12     # LImite superios
